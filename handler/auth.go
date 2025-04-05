aeccd9fe950d4cf23203c06e4b4d6eede9f566b0
mohammad <mh.esfahani95@gmail.com>
2025-04-05 15:42:21 +0330

    generate id and hash password in sign up

--- handler/auth.go
+++ handler/auth.go
@@ -5,7 +5,10 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 	"net/http"
+	"time"
 )
 
 type AuthHandler struct {
@@ -23,19 +26,39 @@ func (a *AuthHandler) SignUp(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewDecoder(r.Body).Decode(&signUpUser)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		errorResponse := entity.ErrorResponse{Message: err.Error()}
-		bytes, _ := json.Marshal(errorResponse)
-		w.Write(bytes)
+		handleErr(err, http.StatusBadRequest, w)
 		return
 	}
 
-	fmt.Printf("User %+v\n", signUpUser)
+	hashPassword, err := bcrypt.GenerateFromPassword([]byte(signUpUser.Password), bcrypt.DefaultCost)
+	if err != nil {
+		handleErr(err, http.StatusBadRequest, w)
+		return
+	}
+
+	signUpUser.Password = string(hashPassword)
+
+	signUpUser.ID = uuid.New().String()
+
 	// save to db
-	//a.db.Query()
+	_, err = a.db.Query("INSERT INTO users (id, first_name, last_name, password, email, created_at, updated_at)"+
+		" VALUES ($1, $2, $3, $4, $5, $6, $7)", signUpUser.ID, signUpUser.FirstName, signUpUser.LastName, signUpUser.Password,
+		signUpUser.Email, time.Now(), time.Now())
+	if err != nil {
+		handleErr(err, http.StatusInternalServerError, w)
+		return
+	}
+
 	json.NewEncoder(w).Encode(signUpUser)
 }
 
 func (a *AuthHandler) SignIn(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Sign In")
+}
+
+func handleErr(err error, status int, w http.ResponseWriter) {
+	w.WriteHeader(status)
+	errorResponse := entity.ErrorResponse{Message: err.Error()}
+	bytes, _ := json.Marshal(errorResponse)
+	w.Write(bytes)
 }
